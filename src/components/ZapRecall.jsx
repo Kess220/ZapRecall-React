d43976f92874bfd210e5983facf3d20c71849a3a
Kaio Victor <prodvictorkaio@gmail.com>
2023-05-18 12:18:50 -0300

    tirando a bibli

--- src/components/ZapRecall.jsx
+++ src/components/ZapRecall.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import imagem from "../assets/logo.png";
-import Pergunta from "./Pergunta";
-import SideBar from "./SideBar";
 import styled from "styled-components";
+import play from "../assets/seta_play.png";
+import virar from "../assets/seta_virar.png";
+import SideBar from "./SideBar.jsx";
 
 const Logo = styled.img`
   width: 52px;
   height: 60px;
 `;
+
 const Nave = styled.div`
   display: flex;
   width: 100%;
@@ -20,24 +22,232 @@ const Nave = styled.div`
 const H1 = styled.h1`
   width: 203.17px;
   height: 44px;
-
   font-family: "Righteous";
   font-size: 20px;
   display: flex;
   align-items: center;
   text-align: center;
-
   color: #ffffff;
-
   transform: rotate(0.58deg);
 `;
+
+const PerguntaWrapper = styled.div`
+  /* Styles for the question wrapper */
+  display: flex;
+  width: 300px;
+  height: 65px;
+  background: #ffffff;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+
+  &.card {
+    width: 299px;
+    height: 131px;
+    background: #ffffd5;
+    box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+    border-radius: 5px;
+    display: flex;
+    align-items: flex-start;
+  }
+`;
+
+const PerguntaNumero = styled.h1`
+  height: 100%;
+  font-family: "Recursive";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+  color: #333333;
+  margin-top: 5px;
+`;
+
+const PlayButton = styled.img`
+  margin-right: 20px;
+  width: 20px;
+  height: 23px;
+`;
+
+const VirarButton = styled.img`
+  /* Styles for the "flip" button */
+  display: flex;
+  align-items: flex-end;
+  width: 30px;
+  height: 20px;
+  margin-top: 105px;
+  margin-right: 10px;
+`;
+
+const RespostaWrapper = styled.div`
+  /* Styles for the answer wrapper */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+`;
+
+const RespostaTexto = styled.p`
+  /* Styles for the answer text */
+  font-family: "Recursive";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+  color: #333333;
+`;
+
+const RespostaBotoes = styled.div`
+  /* Styles for the answer buttons */
+  display: flex;
+  width: 100%;
+  margin-top: 9px;
+  justify-content: space-between;
+`;
+const BotaoZap = styled.button`
+  /* Styles for the answer button */
+  width: 85.17px;
+  height: 37.17px;
+
+  background: #2fbe34;
+  border-radius: 5px;
+  color: #ffffff;
+  cursor: pointer;
+`;
+
+const BotaoQuase = styled.button`
+  /* Styles for the answer button */
+  width: 85.17px;
+  height: 37.17px;
+
+  background: #ff922e;
+  border-radius: 5px;
+  color: #ffffff;
+  cursor: pointer;
+`;
+
+const BotaoNao = styled.button`
+  /* Styles for the answer button */
+  width: 85.17px;
+  height: 37.17px;
+
+  background: #ff3030;
+  border-radius: 5px;
+  color: #ffffff;
+  cursor: pointer;
+`;
+
+const Pergunta = ({
+  pergunta,
+  index,
+  perguntaAtiva,
+  exibirResposta,
+  exibirPergunta,
+  buttonZap,
+}) => {
+  const [zapClicado, setZapClicado] = useState(false);
+  const [respostaRevelada, setRespostaRevelada] = useState(false);
+
+  const revealQuestion = () => {
+    return perguntaAtiva === index;
+  };
+
+  const handleVirar = () => {
+    setRespostaRevelada(!respostaRevelada);
+    console.log(pergunta.resposta);
+  };
+
+  const handleNaoLembrei = () => {
+    // Implement logic for "I don't remember" button here
+  };
+
+  const handleQuaseLembrei = () => {
+    // Implement logic for "I almost remember" button here
+  };
+
+  const handleZap = () => {
+    setZapClicado(true);
+    setRespostaRevelada(false);
+    buttonZap(index);
+  };
+
+  if (
+    !pergunta ||
+    typeof pergunta !== "object" ||
+    !pergunta.resposta ||
+    typeof pergunta.resposta !== "string"
+  ) {
+    return null; // Render something different if the "pergunta" prop is invalid
+  }
+
+  return (
+    <PerguntaWrapper
+      className={`pergunta ${revealQuestion() ? "revealed card" : ""}`}
+    >
+      {revealQuestion() ? (
+        <>
+          <PerguntaNumero>
+            {respostaRevelada ? (
+              <RespostaWrapper>
+                <RespostaTexto>{pergunta.resposta}</RespostaTexto>
+                <RespostaBotoes>
+                  <BotaoNao onClick={handleNaoLembrei}>Não lembrei</BotaoNao>
+                  <BotaoQuase onClick={handleQuaseLembrei}>
+                    Quase lembrei
+                  </BotaoQuase>
+                  <BotaoZap onClick={handleZap}>Zap</BotaoZap>
+                </RespostaBotoes>
+              </RespostaWrapper>
+            ) : (
+              pergunta.pergunta
+            )}
+          </PerguntaNumero>
+          {!respostaRevelada && (
+            <div>
+              <VirarButton
+                className="icone"
+                src={virar}
+                alt="Flip Icon"
+                onClick={handleVirar}
+              />
+            </div>
+          )}
+        </>
+      ) : (
+        <>
+          <PerguntaNumero>{pergunta.numeroPerg}</PerguntaNumero>
+          <PlayButton
+            className="play"
+            src={play}
+            alt="Play Icon"
+            onClick={() => exibirPergunta(index)}
+          />
+        </>
+      )}
+
+      {exibirResposta && revealQuestion() && !respostaRevelada && (
+        <div className="virar">
+          <VirarButton
+            className="icone"
+            src={virar}
+            alt="Flip Icon"
+            onClick={handleVirar}
+          />
+        </div>
+      )}
+    </PerguntaWrapper>
+  );
+};
 
 const ZapRecall = () => {
   const perguntas = [
     {
       numeroPerg: "Pergunta 1",
-      pergunta: "O que é JSX ?",
-      resposta: "JSX é uma sintaxe para escrever HTML dentro do JS ",
+      pergunta: "O que é JSX?",
+      resposta: "JSX é uma sintaxe para escrever HTML dentro do JS",
     },
     {
       numeroPerg: "Pergunta 2",
@@ -47,7 +257,7 @@ const ZapRecall = () => {
     {
       numeroPerg: "Pergunta 3",
       pergunta: "Feijão é por cima ou por baixo do arroz?",
-      resposta: "Feijão por cima, obvio!",
+      resposta: "Feijão por cima, obviamente!",
     },
     {
       numeroPerg: "Pergunta 4",
@@ -75,11 +285,8 @@ const ZapRecall = () => {
   return (
     <>
       <Nave>
-        <Logo src={imagem} alt="Descrição da imagem" />
-
-        <H1>
-          <h1>ZapRecall</h1>
-        </H1>
+        <Logo src={imagem} alt="Image description" />
+        <H1>ZapRecall</H1>
       </Nave>
       <div className="perguntas">
         {perguntas.map((pergunta, index) => (
@@ -94,7 +301,6 @@ const ZapRecall = () => {
           />
         ))}
       </div>
-
       <SideBar />
     </>
   );
